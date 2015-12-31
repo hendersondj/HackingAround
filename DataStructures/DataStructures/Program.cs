6f251145b3e223a13c5a9381fc953c52718e2444
Dave Henderson <dave_henderson1@yahoo.com>
2015-12-30 19:27:17 -0800

    Chnage number 2

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -10,7 +10,7 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            string thing = "asdfasdf";
+            string thing = "asdfafdf";
         }
     }
 }
